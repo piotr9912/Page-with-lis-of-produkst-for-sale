f1bf96a522ce7222aa035150c1847c54f2e280f3
piotr9912 <125743127+piotr9912@users.noreply.github.com>
2026-04-17 09:37:52 +0200

    Add files via upload

---
+++ models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField()
+    cena = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nazwa
